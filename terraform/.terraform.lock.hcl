b69b7d0656e0a726d2985e2fcb3383ba5e16cac8
http:jajek <keinsell@protonmail.com>
2023-03-02 01:12:07 +0100

    feat: add bare `ple8697573` cluster

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/scaleway/scaleway" {
+  version     = "2.12.0"
+  constraints = "2.12.0"
+  hashes = [
+    "h1:VO3y4uQNn0zo2Sa8WapBSObgA30X6lSGjSeiidIuVzs=",
+    "zh:0c14754788deece86f4c2b5ef42eb006bcd42cc35c04c169d6dfd1c9765b1d9c",
+    "zh:14cbaafde2e3b449db593b7249df6061defbc7cce248b5e303884d6ed8191bec",
+    "zh:2d002b31f5bc3a55838150136c037464ef118430aa657ccf2abdac95a79574c7",
+    "zh:4659f39aad566394bef192b14610cd265535771475605fdb91befe0a2aeb3ae3",
+    "zh:4795d86a8fd58cca6e9e06855a9ac843667eb61f0b4d1a8f5ac562e1d2094e4a",
+    "zh:71b7e350adebece0f502bbc1131193413d5c6ee29f3965723e5a61cf7f07a0c1",
+    "zh:89e6b9a5dc65854de7898600ab9f9e12555070e9f80e0040697998238fac9089",
+    "zh:9a5f8198f639ca02da91b67a7c5a34f4c9f5041d62212417407794709eb4c518",
+    "zh:9fe160e976763333e59e284a84130ba34a8f937a07671fd84704d9469c5fe0cc",
+    "zh:c5b7ded911f571fb55768983f0dcdea03f3d18946c56653938a8a6aa6381eef6",
+    "zh:d6b775cc2a09da69a3e98cb675adda04f34ffae790052da9837e12c81c1a7ab4",
+    "zh:de550b7ba438928dba1744733f023f41d8be0865795039a641f31e8c95f880c9",
+    "zh:de9ff5a45f21f87602b926dac09e16d35cef7000dd2211d94ae259ec38126f4e",
+    "zh:f21704de4d191b25a161222e1034d03c2059e49044653746e90eab8a03f55842",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "2.12.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+variable "scaleway_region" {
+  type        = string
+  description = "Scaleway region"
+  default     = "pl-waw"
+}
+
+variable "scaleway_zone" {
+  type        = string
+  description = "Scaleway zone"
+  default     = "pl-waw-1"
+}
+
+variable "scaleway_project" {
+  type        = string
+  description = "Scaleway project id"
+  default     = "d25f8b4c-71cc-463a-9ec9-b193a5d4c04a"
+}
+
+variable "scaleway_access_key" {
+  type        = string
+  description = "Scaleway access key"
+}
+
+variable "scaleway_secret_key" {
+  type        = string
+  description = "Scaleway secret key"
+}
+
+
+provider "scaleway" {
+  region     = var.scaleway_region
+  access_key = var.scaleway_access_key
+  secret_key = var.scaleway_secret_key
+  project_id = var.scaleway_project
+}
+
+
+resource "scaleway_k8s_cluster" "ple8697573" {
+  name                        = "ple8697573"
+  version                     = "1.24.3"
+  cni                         = "cilium"
+  delete_additional_resources = false
+}
+
+resource "scaleway_k8s_pool" "pl5cd46794" {
+  depends_on = [scaleway_k8s_cluster.ple8697573]
+  cluster_id = scaleway_k8s_cluster.ple8697573.id
+  region     = "pl-waw"
+  name       = "pl5cd46794"
+  node_type  = "PLAY2-NANO"
+  size       = 1
+  zone       = var.scaleway_zone
+}
+
