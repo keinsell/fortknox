31289f6cd15e90c820a2e106dde0aba268dca4d0
http:jajek <keinsell@protonmail.com>
2023-03-04 02:02:49 +0100

    feat: output mongodb connection string from terraform (DOPS-13) (#5)

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "kubeconfig" {
   value     = scaleway_k8s_cluster.cluster.kubeconfig
   sensitive = true
 }
+
+output "mongodb_uri" {
+  value     = mongodbatlas_cluster.cluster-test.connection_strings[0].standard_srv
+  sensitive = true
+}
